0ff2b63277fcf09a5ed18a3e33dd7abdf761899c
honey hauwa <hauwa1602@gmail.com>
2024-11-11 14:17:30 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Business from "./Business";
+import Clients from "./Clients"
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import GetStarted from "./GetStarted";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Testimonial from "./Testimonial";
+
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    CTA,
+    Stats,
+    Footer,
+    Hero,
+    Clients,
+    GetStarted,
+    Button,
+    CardDeal,
+    Testimonial,
+    
+
+}
